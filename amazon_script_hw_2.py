4c711c4a957aaf5c5f53fc801fd668102e9a233e
MarinaAz <mariaziz69@gmail.com>
2021-02-06 17:06:32 -0500

    hw_2

---
+++ amazon_script_hw_2.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome(executable_path='/Users/marinaazizbajeva/python-selenium-automation/chromedriver 2')
+
+driver.implicitly_wait(4)
+
+driver.get('https://www.amazon.com/gp/help/customer/display.html')
+
+search_field = driver.find_element(By.ID, 'helpsearch')
+search_field.send_keys('Cancel Order', Keys.ENTER)
+
+actual_text = driver.find_element(By.XPATH, "//b['cancel order']").text
+expected_text = '"Cancel Order"'
+
+assert '"Cancel Order"' == actual_text, f'Expected {expected_text}, but got {actual_text}'
+
+driver.quit()
+
+
+
+
+
+
