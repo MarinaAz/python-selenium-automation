f466285f6b63c76a977e9979eae255279572a40e
MarinaAz <mariaziz69@gmail.com>
2021-03-23 21:45:16 -0400

    HW_5

--- amazon_script.py
+++ amazon_script.py
@@ -1,18 +1,22 @@
 from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 driver = webdriver.Chrome(executable_path='/Users/marinaazizbajeva/python-selenium-automation/chromedriver 2')
 
-driver.implicitly_wait(4)
+# driver.implicitly_wait(4)
 
 driver.get('https://www.amazon.com/')
 
 search_field = driver.find_element(By.ID, 'twotabsearchtextbox')
 search_field.send_keys('Watches')
 
-search_icon = driver.find_element(By.ID, 'nav-search-submit-button')
-search_icon.click()
+driver.wait = WebDriverWait(driver, 10)
+e = driver.wait.until(EC.element_to_be_clickable((By.ID, 'nav-search-submit-button')))
+
+e.click()
 
 actual_text = driver.find_element(By.XPATH, "//span[@class='a-color-state a-text-bold']").text
 expected_text = '"Watches"'
